33aaa7b17705cf7df2608d01ed8122a50c42ab74
Michał Zawitaj <29977@s.pm.szczecin.pl>
2022-12-20 15:10:48 +0100

    plansza.h trochę nowych deklaracji

--- warcaby/plansza.h
+++ warcaby/plansza.h
@@ -2,11 +2,36 @@
 
 #pragma once
 
+
+//definuje tylko jezeli nie jest jeszcze zdefiniowane
 #ifndef PLANSZA_H
 #define PLANSZA_H
 
 
+//dynamicznie powiekszajace sie tablice, brak im optimalizacji przy deklaracji pamieci ale dla tak prostej gry nie bedzie mialo to wiekszego znaczenia
+#include <vector>
 
+//nie chcemy by wielkosc planszy mozna bylo zmieniac
+const int BOARD_SIZE = 8;
+
+
+//klasa pionka
+class Piece {
+public:
+	//ciekawy typ zmiennej ciezki do wytlumaczenia, lista slow z przypisanymi indeksami
+	enum Color { WHITE, BLACK };
+
+	//konstruktor
+	Piece(Color color);
+	//abstrakcyjny destruktor
+	virtual ~Piece(){}
+
+
+	Color getColor() const;
+	char getSymbol() const;
+	bool isKing() const;
+
+};
 
 #endif
 
--- warcaby/warcaby.cpp
+++ warcaby/warcaby.cpp
@@ -7,7 +7,6 @@
 
 /*
 Od dłuższego czasu robiłem wiele kursów, głównie na Udemy żeby podszlifować swoją wiedzę
-
 W tym pliku będe korzystać z wielu nowych rzeczy których nie robiliśmy na zajęciach,
 na swoją obronę (by uniknąć podejrzeń plagiatu) postaram się w komentarzach tłumaczyć wszystkie nowe rzeczy z których korzystałem
 */
@@ -18,7 +17,9 @@ int main()
 {
     //polskie znaki
     setlocale(LC_CTYPE, "Polish");
-    
+    //jeszcze nie wiem czy będę ich używać ale i tak deklaruje, być może znikną w późniejszych wersjach, staram się wszystko pisać po angielsku
+
+
 
 
     //inny zapis return 0/1, moim zdaniem bardziej przejżyste wyjście z programu
