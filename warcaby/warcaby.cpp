44910430c82801562d84cced34e22a90a842d7f5
Michał Zawitaj <29977@s.pm.szczecin.pl>
2022-12-20 19:56:28 +0100

    nowy, prostszy projekt

--- warcaby/warcaby.cpp
+++ warcaby/warcaby.cpp
@@ -1,27 +1,13 @@
 ﻿#include <string>
 #include <iostream>
-//polskie znaki
 #include <conio.h>
-//odwołanie do pliku klasy
-#include "plansza.h"
-
-/*
-Od dłuższego czasu robiłem wiele kursów, głównie na Udemy żeby podszlifować swoją wiedzę
-W tym pliku będe korzystać z wielu nowych rzeczy których nie robiliśmy na zajęciach,
-na swoją obronę (by uniknąć podejrzeń plagiatu) postaram się w komentarzach tłumaczyć wszystkie nowe rzeczy z których korzystałem
-*/
 
 
+using namespace std;
 
 int main()
 {
-    //polskie znaki
-    setlocale(LC_CTYPE, "Polish");
-    //jeszcze nie wiem czy będę ich używać ale i tak deklaruje, być może znikną w późniejszych wersjach, staram się wszystko pisać po angielsku
 
 
-
-
-    //inny zapis return 0/1, moim zdaniem bardziej przejżyste wyjście z programu
     return EXIT_SUCCESS;
 }
