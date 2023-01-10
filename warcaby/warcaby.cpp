20d2a5ee117950a0867256d16bb55a96a27aa5a2
Michał Zawitaj <29977@s.pm.szczecin.pl>
2023-01-10 10:47:59 +0100

    przypisanie pionów planszy oraz sprawdzanie wygranej

--- warcaby/warcaby.cpp
+++ warcaby/warcaby.cpp
@@ -5,9 +5,50 @@
 
 using namespace std;
 
+//1 biale, 2 czarne
+int tab[8][8];
+
+//ilość oraz 0/1 czy jest królem
+int pB = 12;
+int pC = 12;
+
+void start() {
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (i % 2 == 0 && j % 2 == 1 && i < 3 || i % 2 == 1 && j % 2 == 0 && i < 3) {
+                tab[i][j] = 2;
+            }
+            if (i % 2 == 0 && j % 2 == 1 && i > 4 || i % 2 == 1 && j % 2 == 0 && i > 4) {
+                tab[i][j] = 1;
+            }
+        }
+    }
+}
+
+void print() {
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+           cout << tab[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
+
+void checkExit() {
+    for (int i = 0; i < 12; i++) {
+        if (pB == 0) {
+            cout << "Wygrały Czarne!" << endl;
+        } else if (pC == 0) {
+            cout << "Wygrały Białe!" << endl;
+        }
+    }
+}
+
 int main()
 {
-
+    setlocale(LC_CTYPE, "Pollish");
+    start();
+    
 
     return EXIT_SUCCESS;
 }
